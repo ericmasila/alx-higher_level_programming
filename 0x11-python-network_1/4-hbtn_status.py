578c3537aebf638933867dfb357708eff17f6d49
ericmasila <ericmasila254@gmail.com>
2024-03-01 03:28:54 -0800

    correction

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""
-fetch https://intranet.hbtn.io/status; display response
-"""
 import requests
 
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+url = 'https://alx-intranet.hbtn.io/status'
+
+response = requests.get(url)
+body = response.text
+
+print("Body response:")
+print("\t- type:", type(body))
+print("\t- content:", body)
